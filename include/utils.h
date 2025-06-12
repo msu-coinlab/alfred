52646653869af236033b4208b6ac04fbbc1c2590
Ritam Guha <ritamguha16@gmail.com>
2025-06-12 14:02:35 -0400

    update

--- include/utils.h
+++ include/utils.h
@@ -12,5 +12,6 @@ namespace utils {
     unsigned long long get_time();
     std::string format_time(std::time_t time);
     void split_string(const std::string &str, char delim, std::vector<std::string> &out);
+    std::string replace_all(std::string str, const std::string& from, const std::string& to);  // Add this line
 }
 #endif
